0dd8ae86fc7d2111ebd99af8c35e9f948426efe4
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-07 19:36:58 +0100

    more implementation

---
+++ gui/SM.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013, IOhannes m zm?g, IEM
+
+# This file is part of MINTmix
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MINTmix.  If not, see <http://www.gnu.org/licenses/>.
+
+from PySide import QtCore, QtGui
+
+from qsynthMeter import qsynthMeter
+from PySide.QtGui import *
+
+class SM(QtGui.QGroupBox):
+    
+    def __init__(self, parent=None, name="SM??"):
+        super(SM, self).__init__(parent)
+        self.setTitle(name)
+        # Create widgets
+        self.meter = qsynthMeter(self, 4, [])
+        
+        self.iface = QtGui.QComboBox()
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(2,2,2,2)
+
+        layout.addWidget(self.meter)
+        layout.addWidget(self.iface)
+   
+        self.setLayout(layout)
---
+++ gui/SMmixer.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013, IOhannes m zm?g, IEM
+
+# This file is part of MINTmix
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MINTmix.  If not, see <http://www.gnu.org/licenses/>.
+
+from PySide import QtCore, QtGui
+from PySide.QtGui import *
+from SM import SM
+
+class SMmixer(QtGui.QFrame):
+    
+    def __init__(self, parent=None, SMs=None):
+        super(SMmixer, self).__init__(parent)
+        self.sm=[]
+
+        self.layout = QHBoxLayout()
+        self.layout.setSpacing(0)
+        self.setLayout(self.layout)
+        self.build()
+
+    def build(self):
+        self.sm=[]
+
+        while self.layout.count() > 0:
+            item = self.layout.takeAt(0)
+            if not item:
+                continue
+
+            w = item.widget()
+            if w:
+                w.deleteLater()
+
+        # Create widgets
+        count = 0
+##        for sm in SMs:
+##            d=SMs[sm]
+##            print sm
+##            self.sm+=[SM(self, sm)]
+##            self.layout.addWidget(self.sm[count])
+##            count+=1
+        for count in range(2):
+            name="SM"+str(count)
+            print name
+            self.sm+=[SM(self, name)]
+            self.layout.addWidget(self.sm[count])
+            print "SM: ",self.sm[count].sizeHint()
