9363fc32ff39be8a3b24ba8802a68ab26668d3c8
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-07 20:49:04 +0100

    german translation

---
+++ i18n/de_DE.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="de">
+<context>
+    <name>SM</name>
+    <message>
+        <location filename="gui/SM.py" line="63"/>
+        <source>streaming</source>
+        <translation>aktiv</translation>
+    </message>
+</context>
+</TS>
