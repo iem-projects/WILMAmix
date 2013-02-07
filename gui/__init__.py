63dc0689cb45be9bbc0e2299571122244e74c9e5
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-07 19:36:20 +0100

    a gui module

---
+++ gui/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__version__ = "0"
+__author__ = "IOhannes m zmölnig, IEM"
+__license__ = "GNU General Public License"
+__all__ = ["SMmixer",]
+
+from SMmixer import *
