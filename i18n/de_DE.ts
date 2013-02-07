852659bddb2a352cb57cd278338d7bceca12dadb
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-07 21:02:42 +0100

    new translations

--- i18n/de_DE.ts
+++ i18n/de_DE.ts
@@ -1,10 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="de">
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
 <context>
-    <name>SM</name>
+    <name>@default</name>
     <message>
-        <location filename="gui/SM.py" line="63"/>
+        <location filename="gui/SM.py" line="33"/>
         <source>streaming</source>
+        <translatorcomment>tja?</translatorcomment>
         <translation>aktiv</translation>
     </message>
 </context>
