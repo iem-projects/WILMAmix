54b45c81bb21341653c6d0f739ee93faf39e2f3b
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-07 20:48:00 +0100

    translator support

--- gui/__init__.py
+++ gui/__init__.py
@@ -4,6 +4,7 @@
 __version__ = "0"
 __author__ = "IOhannes m zmölnig, IEM"
 __license__ = "GNU General Public License"
-__all__ = ["SMmixer",]
+__all__ = ["SMmixer", "Translator"]
 
 from SMmixer import *
+from Translator import Translator
