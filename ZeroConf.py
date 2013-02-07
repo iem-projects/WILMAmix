967a560069e7720ce51e61962d9e9fa8c15ff2f7
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-07 17:57:35 +0100

    code cleanup

--- ZeroConf.py
+++ ZeroConf.py
@@ -9,7 +9,7 @@ TYPE = '_mintOSC._udp'
 
 if_index2name=[]
 try:
-    import netifacesP
+    import netifaces
     if_index2name = netifaces.interfaces()
 except: pass
 
@@ -22,45 +22,47 @@ def if_indextoname(index):
 
 class ZeroConf:
 
-##    def service_resolved(self, *args):
-##        print "resolved: ", args
-##        print "name: ", args[2]
-##        print "address: %s:%d" % (args[7],args[8])
-##        print "iface: ", args
-##
-##    def print_error(self, *args):
-##        print 'error_handler', args
+    def getKey(self, arg_interface, arg_protocol, arg_name, arg_stype, arg_domain, arg_flags):
+        key = "["+str(arg_interface)+'/'+str(arg_protocol)+'/'+str(arg_name)+'/'+str(arg_stype)+'/'+str(arg_domain)+"]"
+        return key
 
     def newHandler(self, arg_interface, arg_protocol, arg_name, arg_stype, arg_domain, arg_flags):
         interface, protocol, name, stype, domain, host, aprotocol, address, port, txt, flags = self.server.ResolveService(
             arg_interface, arg_protocol, arg_name, arg_stype, arg_domain,
             avahi.PROTO_UNSPEC, dbus.UInt32(0))
+        key=self.getKey(arg_interface, arg_protocol, arg_name, arg_stype, arg_domain, arg_flags)
+        self.dict[key]={'name': name, 'address': address, 'port': port, 'iface': if_indextoname(interface)}
+        
+        
+    def delHandler(self, arg_interface, arg_protocol, arg_name, arg_stype, arg_domain, arg_flags):
+        key=self.getKey(arg_interface, arg_protocol, arg_name, arg_stype, arg_domain, arg_flags)
+        try:
+            del self.dict[key]
+        except:
+            print "removed element not in dict: ", key
 
-        print "name: ", name
-        print "address: %s:%d" % (address, port)
-        print "iface: ", if_indextoname(interface)
-        print ""
-
-##        ifname=if_indextoname(interface)
-##        # the following handlers get called asynchronously
-##        self.server.ResolveService(interface, protocol, name, stype, domain,
-##                                   avahi.PROTO_UNSPEC, dbus.UInt32(0), 
-##                                   reply_handler=self.service_resolved,
-##                                   error_handler=self.print_error)
+        ## FIXXME: print
+        print self.dict
 
     def __init__(self, domain='local'):
+        self.dict=dict()
         loop = DBusGMainLoop()
         bus = dbus.SystemBus(mainloop=loop)
         self.server = dbus.Interface( bus.get_object(avahi.DBUS_NAME, '/'),
                                       avahi.DBUS_INTERFACE_SERVER)
         sbrowser = dbus.Interface(bus.get_object(avahi.DBUS_NAME,
                                                  self.server.ServiceBrowserNew(avahi.IF_UNSPEC,
-                                                                               avahi.PROTO_INET,
+                                                                               avahi.PROTO_UNSPEC,
                                                                                TYPE,
                                                                                domain,
                                                                                dbus.UInt32(0))),
                                   avahi.DBUS_INTERFACE_SERVICE_BROWSER)
         sbrowser.connect_to_signal("ItemNew", self.newHandler)
+        sbrowser.connect_to_signal("ItemRemove", self.delHandler)
+
+    def getDict(self):
+        ## FIXXME: this will return a shallow copy, use deepcopy instead!
+        return self.dict
 
 
 if __name__ == '__main__':
