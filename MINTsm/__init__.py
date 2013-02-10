11f71e62a198e9a0839039f9df9238418a148944
IOhannes m zmoelnig <zmoelnig@umlautO.umlaeute.mur.at>
2013-02-10 19:44:03 +0100

    removed MINTsm
    it's now all in MINT

--- MINTsm/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__version__ = "0"
-__author__ = "IOhannes m zmölnig, IEM"
-__license__ = "GNU General Public License"
-__all__ = ["Publish", ]
-
-from Publish import *
