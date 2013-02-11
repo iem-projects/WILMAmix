b7c5c77b5cd2eb74dec97a163a113ab98ab9a398
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-11 17:30:43 +0100

    export AudioMixer

--- MINT/__init__.py
+++ MINT/__init__.py
@@ -4,7 +4,8 @@
 __version__ = "0"
 __author__ = "IOhannes m zmölnig, IEM"
 __license__ = "GNU General Public License"
-__all__ = ["Discovery", "net"]
+__all__ = ["Discovery", "net", "AudioMixer"]
 
 from Discovery import Publisher, Discoverer
 from net import NetServer, NetClient
+from AudioMixer import AudioMixer
