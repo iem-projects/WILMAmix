d65afb91b44735eb0f75acdefe5855a5010794ce
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-11 16:46:24 +0100

    added 'net'

--- MINT/__init__.py
+++ MINT/__init__.py
@@ -4,6 +4,7 @@
 __version__ = "0"
 __author__ = "IOhannes m zmölnig, IEM"
 __license__ = "GNU General Public License"
-__all__ = ["Discovery", ]
+__all__ = ["Discovery", "net"]
 
-from Discovery import *
+from Discovery import Publisher, Discoverer
+from net import NetServer, NetClient
