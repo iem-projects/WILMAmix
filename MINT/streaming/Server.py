7acace9d9ac09d04773d0348ddc76a0ae1dad39b
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-12 15:49:27 +0100

    Server factory

---
+++ MINT/streaming/Server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2013, IOhannes m zmölnig, IEM
+
+# This file is part of MINTmix
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MINTmix.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def Server(type, profile='L16', source='audiotestsrc'):
+    if 'rtsp' == type:
+        try:
+            import RTSPserver
+            return RTSPserver.RTSPserver(profile, source)
+        except:
+            pass
+
+    return None
+
+if __name__ == '__main__':
+    import time
+    s=Server(type='rtsp')
+
+    s.start()
+    print "URI: ", s.getURI()
+    time.sleep(10)
+    s.stop()
+
+    s.start()
+    print "URI: ", s.getURI()
+    time.sleep(10)
+    s.stop()
+
+    s=Server(type='rtsp')
+    s.start()
+    print "URI: ", s.getURI()
+    time.sleep(10)
+    s.stop()
+    
+    print "bye"
