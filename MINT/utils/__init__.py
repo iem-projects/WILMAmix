40cefc33f10dbef76f9d2cfaec0da9387e286c6d
IOhannes m zmoelnig <zmoelnig@umlautO.umlaeute.mur.at>
2013-02-12 21:53:51 +0100

    constants

--- MINT/utils/__init__.py
+++ MINT/utils/__init__.py
@@ -4,6 +4,7 @@
 __version__ = "0"
 __author__ = "IOhannes m zmölnig, IEM"
 __license__ = "GNU General Public License"
-__all__ = []
+__all__ = ["Constants",]
 
 from mathutils import *
+from Constants import Constants
