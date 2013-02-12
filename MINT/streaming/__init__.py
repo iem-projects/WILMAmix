e908ca6cf7dd1cbb6ab1b377e136b3ac0f71c08a
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-12 15:38:26 +0100

    RTSP server

---
+++ MINT/streaming/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = "0"
+__author__ = "IOhannes m zmölnig, IEM"
+__license__ = "GNU General Public License"
+__all__ = ["RTSP", ]
+
+from RTSPserver import RTSPserver
