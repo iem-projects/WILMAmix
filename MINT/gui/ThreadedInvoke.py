f79669cafa3024ec69c417f2c7b3e854580e1334
IOhannes m zmölnig <zmoelnig@iem.at>
2013-02-18 19:48:45 +0100

    thread-safe invoking of Q-Methods

---
+++ MINT/gui/ThreadedInvoke.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2013, IOhannes m zmölnig, IEM
+
+# This file is part of MINTmix
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MINTmix.  If not, see <http://www.gnu.org/licenses/>.
+
+from PySide import QtCore
+
+class _InvokeEvent(QtCore.QEvent):
+    EVENT_TYPE = QtCore.QEvent.Type(QtCore.QEvent.registerEventType())
+
+    def __init__(self, fn, *args, **kwargs):
+        QtCore.QEvent.__init__(self, _InvokeEvent.EVENT_TYPE)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+
+class _Invoker(QtCore.QObject):
+    def event(self, event):
+        event.fn(*event.args, **event.kwargs)
+
+        return True
+
+_invoker = _Invoker()
+
+
+def callback(fn, *args, **kwargs):
+    QtCore.QCoreApplication.postEvent(_invoker,
+                                      _InvokeEvent(fn, *args, **kwargs))
+
+
+if __name__ == '__main__':
+    def foo(name):
+        print name
+    invoke_in_main_thread(foo, "hello")
