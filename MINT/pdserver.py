9e3e33f78f7dddddb8132177ebd3271279a47b93
IOhannes m zmölnig <zmoelnig@iem.at>
2013-03-19 14:18:06 +0100

    restarting Pd-server

---
+++ MINT/pdserver.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2013, IOhannes m zmölnig, IEM
+
+# This file is part of MINTmix
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MINTmix.  If not, see <http://www.gnu.org/licenses/>.
+
+###############################
+# start a UDP-server and the Pd-workinghorse
+# Pd communicates with use through the UDP-server
+
+from net import server as NetServer
+from net.osc import Bundle
+
+
+from launcher import launcher
+
+import os
+import tempfile
+
+class _pdprocess:
+    def __init__(self, port, cwd=None):
+        if cwd is None:
+            cwd=tempfile.mkdtemp()
+        self.cwd=cwd
+
+        self.args=[]
+        self.args+=['-path', ".:/usr/lib/pd/extra/iemnet:/usr/lib/pd/extra/osc:~/src/cvs/MINT/pd/MINT/iemrtp"]
+        ## NOTE: "sent" messages are executed _after_ loadbang
+        self.args+=['-send', "_MINT_pwd "+cwd]
+        self.args+=['-send', "_MINT_port "+str(port)]
+        self.args+=['-path', "/Net/iem/Benutzer/zmoelnig/src/cvs/MINT/MINTmix/MINT/pd"]
+        #self.args+=['-nogui']
+        self.args+=['-open', "_MINT.pd"]
+        self.shouldRun=False
+        self.pd=None
+        self.start()
+
+    def _launch(self):
+        self.pd = launcher("pd",
+                           self.args,
+                           cwd=self.cwd,
+                           doneCb=self.doneCb)
+        self.pd.launch()
+        self.shouldRun=True
+
+    def doneCb(self):
+        print "Pd exited", self.shouldRun
+        if self.shouldRun: ## ouch: crashed, so restart
+            self._launch()
+    def start(self):
+        if self.pd is None: ## not yet running
+            self._launch()
+    def stop(self):
+        self.shouldRun=False
+        self.pd.shutdown()
+
+class pdserver:
+    def __init__(self):
+        self.server = NetServer(type='udp')
+        self.pd=_pdprocess(self.server.getPort())
+    def __del__(self):
+        self.pd.stop()
+    def stop(self):
+        self.pd.stop()
+
+if __name__ == '__main__':
+    print "Pd-Server..."
+    import gobject
+    gobject.threads_init()
+
+    pd = pdserver()
+
+    try:
+        gobject.MainLoop().run()
+    except KeyboardInterrupt:
+        pd.stop()
+        pass
+
+
