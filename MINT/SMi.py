59987fdeeacca26a4a08eb028d4ce3247983826e
IOhannes m zmölnig <zmoelnig@iem.at>
2013-03-19 20:32:24 +0100

    reorganizing stuff
    in a rather unorganized way

--- MINT/SMi.py
+++ MINT/SMi.py
@@ -21,7 +21,6 @@
 from net import server as NetServer
 from net.osc import Bundle
 
-from launcher import launcher
 import constants
 
 from streaming import Server as StreamingServer
@@ -69,76 +68,83 @@ class State:
         bundle.append(('/state/runtime', self.health.runtime))
 
 class PdCommunicator:
-    def __init__(self, settings, state):
-        self.state=state
-        self.server=pdserver.pdserver(workingdir=settings.outpath, patchdir=settings.inpath)
+    def __init__(self, smi):
+        self.smi=smi
+        self.server=pdserver.pdserver(workingdir=smi.settings['/path/out'], patchdir=smi.settings['/path/in'])
         self.server.add(self._catchall, None)
         self.server.add(self._meter, "/meter")
         self.server.start()
 
     def _meter(self, msg, source):
-        self.state.levels=msg[2:]
-    def _catchall(self, message, source):
-        print "got message: ", message
+        self.smi.state.levels=msg[2:]
+    def _catchall(self, msg, source):
+        self.smi.server.sendMsg(msg[0], msg[2:])
+        print "got message: ", msg
         print "       from: ", source
 
     def stop(self):
         self.server.stop()
 
-class Setting:
-    def __init__(self):
-        self.streamtype='rtsp'
-        self.streamprofile='L16'
-        self.streamchannels=4
-        try:
-            import getpass
-            self.user=getpass.getuser()
-        except ImportError:
-            self.user='unknown'
-        self.outpath='/tmp/MINT/out'
-        self.inpath='/tmp/MINT/in'
-    def addToBundle(self, bundle):
-        bundle.append(('/user', [self.user]))
-        bundle.append(('/path/out', [self.outpath]))
-        bundle.append(('/path/in', [self.inpath]))
+    def send(self, addr, data=None):
+        self.server.send(addr, data)
+
 
 class SMi:
     def __init__(self):
+        self.settings=None
+        self._initSettings()
         self.state=State()
-        self.setting=Setting()
-        self.pd = PdCommunicator(self.setting, self.state)
         self.oscprefix='/'+constants.HOSTNAME
         self.server = NetServer(port=constants.SM_PORT, oscprefix=self.oscprefix, type=constants.PROTOCOL, service=constants.SERVICE)
         self.server.add(self.ping, '/ping')
         self.server.add(self.setGain, '/gain')
+
         self.server.add(self.controlStream, '/stream')
         self.server.add(self.controlStreamType, '/stream/settings/type')
         self.server.add(self.controlStreamProfile, '/stream/settings/profile')
         self.server.add(self.controlStreamChannels, '/stream/settings/channels')
+        self.server.add(self.controlStreamDestination, '/stream/settings/destination')
         self.server.add(self.dumpInfo, '/dump') ## debugging
-        self.launcher=None
-        self.server.add(self.launchCmd, '/launch') ## debugging
         self.mixer = self.state.mixer
-        self.streamer = None
+        self.pd = PdCommunicator(self)
 
     def cleanup(self):
         self.pd.stop()
     def __del__(self):
         self.cleanup()
 
+    def _initSettings(self):
+        d=dict()
+        try:
+            import getpass
+            d['/user']=getpass.getuser()
+        except ImportError:
+            d['/user']='unknown'
+
+        d['/stream/type'    ]='rtp' # const
+        d['/stream/channels']=4 # const
+        d['/stream/profile' ]='L16' # const for now
+
+        d['/path/in'        ]='/tmp/MINT/in'
+        d['/path/out'       ]='/tmp/MINT/out'
+
+        self.settings=d
+
+
     def setGain(self, msg, src):
         if self.mixer is not None:
             gains=self.mixer.gain(msg[2:])
 
     def controlStreamType(self, msg, src):
-        self.setting.streamtype=msg[2]
+        self.settings['/stream/type']=msg[2]
 
     def controlStreamProfile(self, msg, src):
-        self.setting.streamprofile=msg[2]
+        self.settings['/stream/profile']=msg[2]
 
     def controlStreamChannels(self, msg, src):
-        self.setting.streamchannels=msg[2]
-
+        self.settings['/stream/channels']=msg[2]
+    def controlStreamDestination(self, msg, src):
+        self.settings['/stream/destination']=msg[2:4]
 
     def controlStream(self, msg, src):
         state=msg[2]
@@ -153,41 +159,26 @@ class SMi:
 
 
     def startStream(self):
-        if self.streamer is not None:
-            self.stopStream()
-        self.streamer = StreamingServer(type=self.setting.streamtype, profile=self.setting.streamprofile, channels=self.setting.streamchannels,
-                                        startCallback=self.streamStarted)
-        self.streamer.start()
+        self.pd.send("/control/load/stream")
+        self.pd.send("/stream/start", ["localhost", 8888])
 
     def stopStream(self):
-        if self.streamer is not None:
-            self.streamer.stop()
-        self.streamer = None
+        self.pd.send("/stream/stop")
 
     def ping(self, msg, src):
         self.state.update()
         bundle = Bundle(oscprefix=self.oscprefix)
+
         self.state.addToBundle(bundle)
-        self.setting.addToBundle(bundle)
-        bundle.append(('/launch/state', [self.launcher is not None]))
+        for a in ['/user', '/path/in', '/path/out' ]:
+            bundle.append((a, [self.settings[a]]))
         self.server.sendBundle(bundle)
 
     def dumpInfo(self, msg, src):
-        print "setting: ", self.setting.__dict__
+        print "setting: ", self.settings
         print "state  : ", self.state.__dict__
         if self.streamer is not None:
             self.streamer.dumpInfo()
-
-    def launchCmd(self, msg, src):
-        print "launching: ", msg
-        if self.launcher is not None:
-            return
-        self.launcher = launcher(msg[2], cwd='/tmp', doneCb=self.launchCb)
-        self.launcher.start()
-        self.server.sendMsg('/launch/state', [True])
-    def launchCb(self):
-        print "launch callback", self.server
-        self.launcher = None
 
 if __name__ == '__main__':
     print "SMi..."
