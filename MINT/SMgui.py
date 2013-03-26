e9b448bca05c57205cfb68af0ee52bebd1be8c5d
IOhannes m zmölnig <zmoelnig@iem.at>
2013-03-26 18:00:15 +0100

    callbacks for standard OSC messages

--- MINT/SMgui.py
+++ MINT/SMgui.py
@@ -20,23 +20,57 @@
 
 ## that's the main instance for SMi's GUI
 
-from gui import SMconfig, SMchannels, ThreadedInvoke
 import configuration, filesync
+from gui import SMconfig, SMchannels, ThreadedInvoke
+from net import client as _NetClient
+
 import os
 
 class SMgui:
     def __init__(self, parent=None, name="SMi", confs=None, maxwidth=None):
-        self.settings=configuration.getSM(name)
-        self.confs=confs
+        oscprefix=name
+        while oscprefix.startswith('/'):
+            oscprefix=oscprefix[1:]
         interfaces=[]
         if confs is not None:
             interfaces=sorted(confs.keys())
+
+        self.settings=configuration.getSM(name)
+        self.confs=confs
+        self.connection=None
+        self.critical=[False]*5
+
+        try:
+            defaultconf=interfaces[0]
+            config = confs[defaultconf]
+            self.settings['/host'] = config['address']
+            self.connection = _NetClient(config['address'],
+                                         config['port'],
+                                         oscprefix='/'+oscprefix,
+                                         type=self.settings['/protocol'])
+            self._connect()
+        except IndexError:
+            print "no network configurations -> no connection"
+
         self.channels=SMchannels.SMchannels(self, guiparent=parent, settings=self.settings, maxwidth=maxwidth)
         self.config=SMconfig.SMconfig(self, guiparent=parent, settings=self.settings, interfaces=interfaces)
         self.name = name
 
         if confs is not None:
             print "FIXXME: confs not yet used in SMgui"
+
+    def _connect(self):
+        self.connection.add(self._smiUser,      '/user')
+        self.connection.add(self._smiOutpath,   '/path/out')
+        self.connection.add(self._smiInpath,    '/path/in')
+        self.connection.add(self._smiFader,     '/gain')
+        self.connection.add(self._smiLevel,     '/level')
+        self.connection.add(self._smiStreamURI, '/stream/uri')
+        self.connection.add(self._smiStateCpu,  '/state/cpu')
+        self.connection.add(self._smiStateMem,  '/state/mem')
+        self.connection.add(self._smiStateDisk, '/state/disk')
+        self.connection.add(self._smiStateBatt, '/state/battery')
+        self.connection.add(self._smiStateRuntime, '/state/runtime')
 
     def widget(self):
         return self.channels
@@ -53,8 +87,7 @@ class SMgui:
         return self.isChecked()
 
     def ping(self):
-        ## FIXME: compat implementation for SM.py
-        pass
+        self.connection.sendMsg('/ping')
 
     ## callbacks from childs (SMchannels/SMconfig)
     def showConfig(self):
@@ -82,7 +115,48 @@ class SMgui:
                             deleteTarget=True,
                             doneCallback=ThreadedInvoke.Invoker(self.config.pushEnable))
     def send(self, msg, data=None):
-        print "FIXME: send"
+        self.connection.send(msg, data)
+
+    def _smiUser(self, msg, src):
+        self.settings['/user']=msg[2]
+    def _smiFader(self, msg, src):
+        self.config.setFader(msg[2])
+    def _smiLevel(self, msg, src):
+        levels=msg[2:]
+        self.channels.setLevels(levels)
+        self.config.setLevels  (levels)
+    def _smiOutpath(self, msg, src):
+        self.settings['/path/out']=msg[2]
+    def _smiInpath (self, msg, src):
+        self.settings['/path/in']=msg[2]
+    def _smiStreamURI(self, msg, src):
+        print "FIXME: smiURI"
+    def _smiStateCpu(self, msg, src):
+        value=msg[2]
+        index=0
+        self.config.setState(index, value)
+        self.critical[index]=value>0.9
+    def _smiStateMem(self, msg, src):
+        value=msg[2]
+        index=1
+        self.config.setState(index, value)
+        self.critical[index]=value>0.9
+    def _smiStateDisk(self, msg, src):
+        value=msg[2]
+        index=2
+        self.config.setState(index, value)
+        self.critical[index]=value>0.9
+    def _smiStateBatt(self, msg, src):
+        value=msg[2]
+        index=3
+        self.config.setState(index, value)
+        self.critical[index]=value<0.1
+    def _smiStateRuntime(self, msg, src):
+        value=msg[2]
+        index=4
+        self.config.setState(index, value)
+        self.critical[index]=value<10
+
 
 ######################################################################
 if __name__ == '__main__':
