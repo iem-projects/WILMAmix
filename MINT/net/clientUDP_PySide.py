d9b33daf2396c47a6c01e8f74c1355d79386da7b
IOhannes m zmölnig <zmoelnig@iem.at>
2013-04-02 12:00:02 +0200

    make clientUDP_PySide a child-class of clientAbstract

--- MINT/net/clientUDP_PySide.py
+++ MINT/net/clientUDP_PySide.py
@@ -18,27 +18,24 @@
 # You should have received a copy of the GNU General Public License
 # along with MINTmix.  If not, see <http://www.gnu.org/licenses/>.
 
-import osc
+import clientAbstract
 from PySide.QtNetwork import QUdpSocket
 
-class clientUDP:
+class clientUDP(clientAbstract.clientAbstract):
     """ OSC-client running on GOD.
     sends OSC-messages to SMi.
     receives OSC-messages from SMi (and emits signals with the data)
     """
 
     def __init__(self, host, port, oscprefix='', verbose=False):
-        self.addressManager = osc.CallbackManager(verbose=verbose)
+        super(clientUDP, self).__init__(oscprefix=oscprefix, verbose=verbose)
         self.keepListening=True
-        self.oscPrefix=oscprefix
         self.verbose=verbose
-        self.remote =None
 
         self.socket = QUdpSocket()
         self.socket.readyRead.connect(self._callback)
         self.socket.connectToHost(host, port);
-        self.remote = (host, port) ## FIXXME: 'host' is not canonicalized
-
+        self.remote = (self.socket.peerName(), self.socket.peerPort())
 
     def __del__(self):
         self.shutdown()
@@ -63,15 +60,6 @@ class clientUDP:
             datagram, sender, senderPort = self.socket.readDatagram(self.socket.pendingDatagramSize())
             self.addressManager.handle(datagram.data(), (sender.toString(), senderPort))
 
-    def add(self, callback, oscAddress):
-        """add a callback for oscAddress"""
-        if self.addressManager is not None:
-            if oscAddress is None:
-                if self.oscPrefix is not '': oscAddress = self.oscPrefix+'/'
-            else:
-                oscAddress=self.oscPrefix+oscAddress
-            self.addressManager.add(callback, oscAddress)
-
     def _send(self, data):
         from PySide.QtNetwork import QHostAddress
         if self.socket is not None and self.remote is not None:
@@ -79,28 +67,6 @@ class clientUDP:
                 print "sending '", data, "' to ", self.remote
 
             self.socket.writeDatagram(data, QHostAddress(self.remote[0]), self.remote[1])
-
-    def sendMsg(self, oscAddress, dataArray=[]):
-        """send an OSC-message to the server"""
-        self._send( osc.createBinaryMsg(self.oscPrefix+oscAddress, dataArray) )
-    def sendBundle(self, bundle):
-        """send an OSC-bundle to the server"""
-        if isinstance(bundle, osc.Bundle):
-            self._send(bundle.data())
-        else:
-            self._send(bundle.message)
-    def send(self, addr, data=None):
-        if type(addr) is str: # it's an addr/data pair
-            self.sendMsg(addr, data)
-        elif data is None:    # it's a bundle
-            self.sendBundle(addr)
-        else:
-            raise Exception("usage: send(addr, data) OR send(bundle)")
-
-    def getRemote(self):
-        return self.remote
-
-
 
 ######################################################################
 
