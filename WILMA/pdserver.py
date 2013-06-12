469dc1e6cd609bf70156862c9b6d4884ca1d3e1b
IOhannes m zmölnig <zmoelnig@iem.at>
2013-06-12 14:33:31 +0200

    implementing pdserver's removeAll()

--- WILMA/pdserver.py
+++ WILMA/pdserver.py
@@ -84,10 +84,14 @@ class _pdprocess:
 
 class pdserver:
     def __init__(self, mainpatch='MAIN.pd', workingdir=None, patchdir=None, backend=None):
-        self.server = NetServer(transport='udp')
-        self.server.add(self._ping, '/ping')
-        self.pd=_pdprocess(self.server.getPort(), patch=mainpatch, cwd=workingdir, cpd=patchdir, runningCb=self._runningCb)
         self.stateCb = None
+        self.server  = None
+        self.pd      = None
+
+        self.server = NetServer(transport='udp')
+        self.removeAll()
+        self.pd=_pdprocess(self.server.getPort(), patch=mainpatch, cwd=workingdir, cpd=patchdir, runningCb=self._runningCb)
+
     def __del__(self):
         self.pd.stop()
 
@@ -113,10 +117,17 @@ class pdserver:
         self.server.add(callback, oscAddress)
     def send(self, addr, data=None):
         self.server.send(addr, data)
+    def removeAll(self):
+        ## FIXXME: this removes self.server-internal hooks like /ping
+        print "pdserver.removell", self
+        self.server.removeAll()
+        self.server.add(self._ping, '/ping')
+        self.stateCb = None
+
     def ping(self):
         self.server.send('/ping', [])
     def _ping(self, addr, typetags, data, source):
-        self.server.sendMsg('/pong', data)        
+        self.server.sendMsg('/pong', data)
 
 if __name__ == '__main__':
     class PingPong:
@@ -139,5 +150,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         pd.stop()
         pass
-
-
