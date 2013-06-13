e1e8d4e68d6cb706527fecb2453a37f98fba7306
IOhannes m zmölnig <zmoelnig@iem.at>
2013-06-13 15:27:21 +0200

    regenerated SMchannel UI
    after the name change

--- WILMA/gui/SMchannel_ui.py
+++ WILMA/gui/SMchannel_ui.py
@@ -1,44 +1,44 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'SMchannels.ui'
+# Form implementation generated from reading ui file 'SMchannel.ui'
 #
-# Created: Wed Apr  3 13:18:25 2013
-#      by: pyside-uic 0.2.13 running on PySide 1.1.1
+# Created: Thu Jun 13 15:23:55 2013
+#      by: pyside-uic 0.2.14 running on PySide 1.1.1
 #
 # WARNING! All changes made in this file will be lost!
 
 from PySide import QtCore, QtGui
 
-class Ui_SMchannels(object):
-    def setupUi(self, SMchannels):
-        SMchannels.setObjectName("SMchannels")
-        SMchannels.resize(144, 376)
+class Ui_SMchannel(object):
+    def setupUi(self, SMchannel):
+        SMchannel.setObjectName("SMchannel")
+        SMchannel.resize(144, 376)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(SMchannels.sizePolicy().hasHeightForWidth())
-        SMchannels.setSizePolicy(sizePolicy)
-        SMchannels.setCheckable(True)
-        self.verticalLayout = QtGui.QVBoxLayout(SMchannels)
+        sizePolicy.setHeightForWidth(SMchannel.sizePolicy().hasHeightForWidth())
+        SMchannel.setSizePolicy(sizePolicy)
+        SMchannel.setCheckable(True)
+        self.verticalLayout = QtGui.QVBoxLayout(SMchannel)
         self.verticalLayout.setContentsMargins(2, 2, 2, 2)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.meter = qsynthMeter(SMchannels)
+        self.meter = qsynthMeter(SMchannel)
         self.meter.setObjectName("meter")
         self.verticalLayout.addWidget(self.meter)
-        self.launchButton = QtGui.QPushButton(SMchannels)
+        self.launchButton = QtGui.QPushButton(SMchannel)
         self.launchButton.setCheckable(True)
         self.launchButton.setObjectName("launchButton")
         self.verticalLayout.addWidget(self.launchButton)
-        self.configButton = QtGui.QPushButton(SMchannels)
+        self.configButton = QtGui.QPushButton(SMchannel)
         self.configButton.setObjectName("configButton")
         self.verticalLayout.addWidget(self.configButton)
 
-        self.retranslateUi(SMchannels)
+        self.retranslateUi(SMchannel)
 
-    def retranslateUi(self, SMchannels):
-        SMchannels.setWindowTitle(QtGui.QApplication.translate("SMchannels", "SMi", None, QtGui.QApplication.UnicodeUTF8))
-        SMchannels.setTitle(QtGui.QApplication.translate("SMchannels", "SMi", None, QtGui.QApplication.UnicodeUTF8))
-        self.launchButton.setText(QtGui.QApplication.translate("SMchannels", "START", None, QtGui.QApplication.UnicodeUTF8))
-        self.configButton.setText(QtGui.QApplication.translate("SMchannels", "Config", None, QtGui.QApplication.UnicodeUTF8))
+    def retranslateUi(self, SMchannel):
+        SMchannel.setWindowTitle(QtGui.QApplication.translate("SMchannel", "SMi", None, QtGui.QApplication.UnicodeUTF8))
+        SMchannel.setTitle(QtGui.QApplication.translate("SMchannel", "SMi", None, QtGui.QApplication.UnicodeUTF8))
+        self.launchButton.setText(QtGui.QApplication.translate("SMchannel", "START", None, QtGui.QApplication.UnicodeUTF8))
+        self.configButton.setText(QtGui.QApplication.translate("SMchannel", "Config", None, QtGui.QApplication.UnicodeUTF8))
 
 from qsynthMeter import qsynthMeter
