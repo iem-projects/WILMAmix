702a9837848ce8686cdafb952acac727bad1d868
IEM@beaglebone <zmoelnig@iem.at>
2013-06-18 12:59:36 +0200

    more link scripts

---
+++ WILMA/pd/lib/link_net.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+FILES="\
+	udpclient.pd_linux \
+	udpreceive.pd_linux \
+	udpsend.pd_linux \
+	udpserver.pd_linux \
+	libiemnet.so"
+
+. ./link_common
---
+++ WILMA/pd/lib/link_osc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+FILES="\
+	packOSC.pd_linux \
+	pipelist.pd_linux \
+	routeOSC.pd_linux \
+	unpackOSC.pd_linux"
+
+. ./link_common
