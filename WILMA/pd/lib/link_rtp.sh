4ed55513298649bb9dea54202fdf630a2ed07458
IEM@beaglebone <zmoelnig@iem.at>
2013-06-18 12:01:57 +0200

    scripts to create symlinks for iemrtp

---
+++ WILMA/pd/lib/link_rtp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+FILES="rtpL16pay~.pd_linux \
+	rtpL16depay~.pd \
+	L16decode.pd_linux \
+	rtpdepay~.pd \
+	packRTCP.pd_linux \
+	unpackRTCP.pd_linux \
+	unpackRTP.pd_linux \
+	libiemrtp.so"
+
+. ./link_common
