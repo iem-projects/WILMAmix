df9d3922a5b135bd4a964972458405aaa132140e
IOhannes m zmölnig <zmoelnig@iem.at>
2013-06-19 11:54:10 +0200

    more complex logging

--- WILMA/logger.py
+++ WILMA/logger.py
@@ -18,28 +18,35 @@
 # You should have received a copy of the GNU General Public License
 # along with WILMix.  If not, see <http://www.gnu.org/licenses/>.
 
-import logging
+import logging, logging.handlers
 
 class logger:
     def __init__(self, name=None):
-        formatter = logging.Formatter('%(levelname)s(%(name)s): %(message)s')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s(%(name)s): %(message)s')
         lh = None
         if name is not None:
             lh = logging.FileHandler(name+'.log')
         if lh is not None:
             lh.setFormatter(formatter)
+        self.lh = lh
 
         logger = logging.getLogger('WILMA')
-        logger.propagate = (name is None)
-        logger.setLevel(logging.INFO)
-        if lh is not None:
-            logger.addHandler(lh)
-
         osclogger = logging.getLogger('OSC')
-        osclogger.setLevel(logging.INFO)
-        osclogger.propagate = (name is None)
-        if lh is not None:
-            osclogger.addHandler(lh)
+
+        for l in [logger, osclogger]:
+            l.propagate = (name is None)
+            l.setLevel(logging.INFO)
+            if lh is not None:
+                l.addHandler(lh)
+##            l.addHandler(logging.handlers.SysLogHandler(address='/dev/log'))
+
+    def getFiles(self):
+        try:
+            return [self.lh.stream]
+        except:
+            pass
+        return None
+
 
 ####################################################
 
