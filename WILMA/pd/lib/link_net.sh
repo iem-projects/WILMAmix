4bfc8fe74f3f943750e9f9c21455f0cac0c27630
IEM@beaglebone <zmoelnig@iem.at>
2013-06-24 10:31:39 +0200

    add TCP objects

--- WILMA/pd/lib/link_net.sh
+++ WILMA/pd/lib/link_net.sh
@@ -5,6 +5,10 @@ FILES="\
 	udpreceive.pd_linux \
 	udpsend.pd_linux \
 	udpserver.pd_linux \
+	tcpclient.pd_linux \
+	tcpreceive.pd_linux \
+	tcpsend.pd_linux \
+	tcpserver.pd_linux \
 	libiemnet.so"
 
 . ./link_common
