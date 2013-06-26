d6dcc47f334038c8feb473ddf2b057b75c7f2621
IOhannes m zmölnig <zmoelnig@iem.at>
2013-06-26 14:25:22 +0200

    allow to initialize configuration with defaults-dict()

--- WILMA/configuration.py
+++ WILMA/configuration.py
@@ -77,62 +77,73 @@ def _setDict(config, section, values):
 ## DEFAULT values
 # use STRING type for everything (even for numbers)
 # they will be converted to numbers automatically in the _getDict() functions
-_defaults=dict()
-_defaults['/id'      ] = socket.gethostname()
-_defaults['/host'    ] = socket.gethostname()
-
-try:
-    import getpass
-    _defaults['/user']=getpass.getuser()
-except ImportError:
-    _defaults['/user']='unknown'
-_defaults['/passphrase'] = '****'
-
-_defaults['/protocol'] = 'udp'
-_defaults['/service' ] = '_wilma-sm'
-_defaults['/port'    ] = '7777' # LATER change to 0
-_defaults['/gain_control'] = '4' ## alsa control for mic amp
-
-_defaults['/path/in' ] = '/tmp/WILMA/in'
-_defaults['/path/out'] = '/tmp/WILMA/out'
-
-_defaults['/mode'           ]='stream' # 'stream', 'record', 'process'
-_defaults['/stream/transport/protocol']='udp'
-_defaults['/stream/transport/port']='0'
-_defaults['/stream/protocol']='rtp' # const
-_defaults['/stream/profile' ]='L16' # const for now
-_defaults['/stream/channels']='4' # const
-
-
-_defaults['/record/timestamp']='0' # const
-_defaults['/record/filename' ]='WILMA' # const
-
-
-_defaults['/network/interface']='eth0' # ????
-
-## proxy settings
-_defaults['/proxy/server/port']=0
-_defaults['/proxy/client/port']=0
-_defaults['/proxy/client/host']='localhost'
-
-
-_config = ConfigParser.ConfigParser(_defaults)
-_config.read(_configfiles)
-_mixConf=_getDict(_config, 'MIX')
-
-_smDefaults=_getDict(_config, 'SM')
-_config = ConfigParser.ConfigParser(_smDefaults)
-_config.read(_configfiles)
-_smConf=_getDict(_config, _smDefaults['/id'])
-_smConf['/id']=_smDefaults['/id']
-
+_config=None
+_smConf=None
 _smConfs=dict()
+_mixConf=None
+_smDefaults=None
 
+def init(defaults={}):
+    global _config, _smDefaults, _smConf, _smConfs, _mixConf
+    _smConfs=dict()
+    mydefaults=dict()
+    mydefaults['/id'      ] = socket.gethostname()
+    mydefaults['/host'    ] = socket.gethostname()
+
+    try:
+        import getpass
+        mydefaults['/user']=getpass.getuser()
+    except ImportError:
+        mydefaults['/user']='unknown'
+
+    mydefaults['/passphrase'] = '****'
+
+    mydefaults['/protocol'] = 'udp'
+    mydefaults['/service' ] = '_wilma-sm'
+    mydefaults['/port'    ] = '7777' # LATER change to 0
+    mydefaults['/gain_control'] = '4' ## alsa control for mic amp
+
+    mydefaults['/path/in' ] = '/tmp/WILMA/in'
+    mydefaults['/path/out'] = '/tmp/WILMA/out'
+
+    mydefaults['/mode'           ]='stream' # 'stream', 'record', 'process'
+    mydefaults['/stream/transport/protocol']='udp'
+    mydefaults['/stream/transport/port']='0'
+    mydefaults['/stream/protocol']='rtp' # const
+    mydefaults['/stream/profile' ]='L16' # const for now
+    mydefaults['/stream/channels']='4' # const
+
+
+    mydefaults['/record/timestamp']='0' # const
+    mydefaults['/record/filename' ]='WILMA' # const
+
+
+    mydefaults['/network/interface']='eth0' # ????
+
+    ## proxy settings
+    mydefaults['/proxy/server/port']=0
+    mydefaults['/proxy/client/port']=0
+    mydefaults['/proxy/client/host']='localhost'
+
+    for k in defaults:
+        mydefaults[k]=defaults[k]
+
+    _config = ConfigParser.ConfigParser(mydefaults)
+    _config.read(_configfiles)
+    _mixConf=_getDict(_config, 'MIX')
+
+    _smDefaults=_getDict(_config, 'SM')
+    _config = ConfigParser.ConfigParser(_smDefaults)
+    _config.read(_configfiles)
+    _smConf=_getDict(_config, _smDefaults['/id'])
+    _smConf['/id']=_smDefaults['/id']
 
 ###
 # public accessors
 def getSM(id=None):
     """get config dict for WILMAsm"""
+    if not _smConf:
+        init()
     if (id is None) or (id == _smConf['/id']):
         logging.info("default SMconf %s" % id)
         return _smConf
@@ -147,9 +158,14 @@ def getSM(id=None):
 
 def getMIX():
     """get config dict for WILMix"""
+    if not _mixConf:
+        init()
     return _mixConf
 
 def write(name):
+    if (not _smConf) or (not _smConf) or (not _smDefaults):
+        init()
+
     config=ConfigParser.ConfigParser()
     _setDict(config, 'MIX', _mixConf)
     _setDict(config, 'SM' , _smDefaults)
