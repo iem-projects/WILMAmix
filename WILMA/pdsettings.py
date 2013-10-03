7dec30d4291266de71cb96f4f26d2e26857983be
IOhannes m zmölnig <zmoelnig@iem.at>
2013-10-03 17:59:56 +0200

    removing "pdfile" leftovers

--- WILMA/pdsettings.py
+++ WILMA/pdsettings.py
@@ -488,50 +488,6 @@ def parseFile(filename, result=dict()):
 
     return result
 
-class pdsettings:
-    def __init__(self, filename):
-        with open(filename, 'r') as f:
-            content = f.read()
-        content=content.replace('\\;', '')
-        content=content.replace('\n', '')
-        content=content.replace('\r', '')
-        self.content = content.split(';')
-        self.inlets=(0,0)
-        self.outlets=(0,0)
-        self._parse()
-
-    def _parse(self):
-        depth=0
-        re_inlet  =re.compile('^#X obj [0-9]+ [0-9]+ inlet( .*)?$')
-        re_inletS =re.compile('^#X obj [0-9]+ [0-9]+ inlet~( .*)?$')
-        re_outlet  =re.compile('^#X obj [0-9]+ [0-9]+ outlet( .*)?$')
-        re_outletS =re.compile('^#X obj [0-9]+ [0-9]+ outlet~( .*)?$')
-
-        n_inlet=0
-        n_outlet=0
-        n_inletS=0
-        n_outletS=0
-        for l in self.content:
-            if l.startswith('#N canvas'):
-                depth+=1
-            elif l.startswith('#X restore'):
-                depth-=1
-            if depth is 1:
-                if re_inlet.search(l) is not None:
-                    n_inlet+=1
-                elif re_inletS.search(l) is not None:
-                    n_inletS+=1
-                elif re_outlet.search(l) is not None:
-                    n_outlet+=1
-                elif re_outletS.search(l) is not None:
-                    n_outletS+=1
-        self.inlets=(n_inlet, n_inletS)
-        self.outlets=(n_outlet, n_outletS)
-
-    def getInlets(self):
-        return self.inlets
-    def getOutlets(self):
-        return self.outlets
 
 ######################################################################
 
