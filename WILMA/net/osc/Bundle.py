2e20d9b401ca30eb1aa1245301b0b6806849f2da
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2013-11-15 15:50:28 +0100

    allow direct appending of OSCMessage to Bundle

--- WILMA/net/osc/Bundle.py
+++ WILMA/net/osc/Bundle.py
@@ -39,15 +39,18 @@ class Bundle(object):
         if self.prefix is None:
             self.prefix=''
     def append(self, message):
-        m = OSCMessage()
-        address=self.prefix+message[0]
-        data=message[1]
-        m.address = address
-        try:
-            for x in data:
-                m.append(x)
-        except TypeError:
-            m.append(data)
+        if isinstance(message, OSCMessage):
+            m=message
+        else:
+            m = OSCMessage()
+            address=self.prefix+message[0]
+            data=message[1]
+            m.address = address
+            try:
+                for x in data:
+                    m.append(x)
+            except TypeError:
+                m.append(data)
         self.b.append(m.getBinary(), 'b')
         return self
     def __iadd__(self, message):
