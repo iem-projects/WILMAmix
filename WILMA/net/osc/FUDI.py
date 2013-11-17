1594393bc5cc26b4bd808691bc1cd0da6f80e2f4
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2013-11-17 21:19:28 +0100

    kind of working OSC->FUDI converter

--- WILMA/net/osc/FUDI.py
+++ WILMA/net/osc/FUDI.py
@@ -21,6 +21,12 @@
 import OSC
 from Bundle import Bundle as OSCBundle
 
+## TODO: bundle should be converted to nested arrays
+
+## TODO: handle timetags in bundles
+## TODO: escape FUDI special characters (space, semicolon,...)
+
+
 class FUDI(object):
     def __init__(self):
         self.fudiarray=[]
@@ -28,28 +34,38 @@ class FUDI(object):
         self.manager.add(self._fudiMsg, None)
         self.manager.add(self._fudiBundle, "#bundle")
     def _fudiMsg(self, addr, typetags, data, src):
-        self.fudiarray+=[addr[0]]
-        self.fudiarray+=data
+        s=addr[0]
+        for a in data:
+            s+=" "
+            if isinstance(a, basestring):
+                s+=a.replace(' ', '\\ ').replace(';', '\\;').replace(',', '\\,')
+            elif a is None:
+                s+="bang"
+            elif a is True:
+                s+="1"
+            elif a is False:
+                s+="0"
+            else:
+                s+=str(a)
+        self.fudiarray+=[s+";"]
     def _fudiBundle(self, timetag, starting, depth, source):
         if starting:
-            self.fudiarray+=['[']
-            self.fudiarray+=[depth]
-            if timetag:
-                self.fudiarray+=[timetag]
+            s="[ %d" % depth
+#            if timetag:
+#                s+=" %s" % timetag
+#                self.fudiarray+=[timetag]
+            s+=";"
         else:
-            self.fudiarray+=[']']
-            self.fudiarray+=[depth]
+            s="] %d;" % depth
+        self.fudiarray+=[s]
 
     def getFUDI(self, msg):
         self.fudiarray=[]
-        try:
-            self.manager.handle(msg.data())
-        except:
-            self.manager.handle(msg.message)
+        self.manager.handle(msg.data())
         return self.fudiarray
 
 if __name__ == "__main__":
-    print("Welcome to the OSC/FUDI testing program.")
+    #    print("Welcome to the OSC/FUDI testing program.")
     def getFUDI(msg):
         f=FUDI()
         return f.getFUDI(msg)
@@ -59,15 +75,15 @@ if __name__ == "__main__":
         message.append(44)
         message.append(11)
         message.append(4.5)
-        message.append(True)
-        message.append(None)
-        message.append("the white cliffs of dover")
-        print("MSG: %s" % getFUDI(message))
+        #message.append(True)
+        #message.append(None)
+        message.append("the white shiny cliffs of dover")
+        for a in getFUDI(message):  print a
 
-        print("msg: %s %s" % (isinstance(message, OSC.OSCMessage), type(message)))
         bundle = OSCBundle()
         bundle.append(message)
         bundle+=message
-        print("bundle: %s" % getFUDI(bundle))
+        #print("bundle: %s" % getFUDI(bundle))
+        for a in getFUDI(bundle):  print a
 
     testFUDI()
